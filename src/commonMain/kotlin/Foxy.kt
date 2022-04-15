831f05b6fd4f30a1afee11415b4e77fa5a9759e2
Nodar Sotkilava <nsotkilava@outlook.com>
2022-04-15 16:45:52 -0400

    Fixed validation lifespan to pass testOAuthFlow

--- src/commonMain/kotlin/Foxy.kt
+++ src/commonMain/kotlin/Foxy.kt
@@ -1,3 +1,15 @@
+/*
+ * Foxy.kt
+ * Copyright (C) 2022 Hyperspace Developers.
+ * This file is part of project Foxy.
+ *
+ * The Foxy project is non-violent software: you can use, redistribute, and/or modify it under the terms of the NPLv7+
+ * as found in the LICENSE file in the source code root directory or at <https://git.pixie.town/thufie/npl-builder>.
+ *
+ * The Foxy project comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law.  See the
+ * NPL for details.
+ */
+
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
@@ -8,9 +20,7 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toInstant
-import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.Instant
 import kotlinx.serialization.json.Json
 import models.Application
 import models.Token
@@ -128,13 +138,11 @@ class Foxy(var domain: String = "mastodon.social") {
 
             }
         ).body<Token>()
-
+        
         session =
             ValidatedSession(
                 tokenEntity.accessToken,
-                tokenEntity.createdAt
-                    .toInstant()
-                    .toLocalDateTime(TimeZone.UTC)
+                Instant.fromEpochSeconds(tokenEntity.createdAt.toLong())
                     .toString()
             )
         //FIXME: A user should be able to change the lifespan of their token but the default should be 2 weeks.
