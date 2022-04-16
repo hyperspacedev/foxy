e078a5bf0bdb777cca22c7c6f736fa7ed5da6f50
Marquis Kurt <software@marquiskurt.net>
2022-04-16 15:28:01 -0400

    Added ScheduledStatus entity and its sub-entities
    Sub-entities: ScheduledStatusParameters
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

---
+++ src/commonMain/kotlin/models/ScheduledStatus.kt
@@ -0,0 +1,34 @@
+/*
+ * ScheduledStatus.kt
+ * Copyright (C) 2022 Hyperspace Developers.
+ * This file is part of project Foxy.
+ *
+ * The Foxy project is non-violent software: you can use, redistribute, and/or modify it under the terms of the NPLv7+
+ * as found in the LICENSE file in the source code root directory or at <https://git.pixie.town/thufie/npl-builder>.
+ *
+ * The Foxy project comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law.  See the
+ * NPL for details.
+ */
+
+package models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+/** A data class that represents a scheduled status that will be posted in the future. */
+@Serializable
+data class ScheduledStatus(
+
+    /** The scheduled status's ID. */
+    var id: String,
+
+    /** An ISO-8061 string that states when the status will be posted. */
+    @SerialName("scheduled_at") var scheduledAt: String,
+
+    /** The parameters for the scheduled status upon creation. */
+    val parameters: ScheduledStatusParameters,
+
+    /** A list of the media attachments for the status. */
+    @SerialName("media_attachments") val mediaAttachments: List<Attachment>
+)
---
+++ src/commonMain/kotlin/models/ScheduledStatusParameters.kt
@@ -0,0 +1,46 @@
+/*
+ * ScheduledStatusParameters.kt
+ * Copyright (C) 2022 Hyperspace Developers.
+ * This file is part of project Foxy.
+ *
+ * The Foxy project is non-violent software: you can use, redistribute, and/or modify it under the terms of the NPLv7+
+ * as found in the LICENSE file in the source code root directory or at <https://git.pixie.town/thufie/npl-builder>.
+ *
+ * The Foxy project comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law.  See the
+ * NPL for details.
+ */
+
+package models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+/** A data class representing the parameters for creating a status. */
+@Serializable
+data class ScheduledStatusParameters(
+
+    /** The text content of the scheduled status. */
+    val text: String,
+
+    /** The ID of the account the status replies to, if it is a reply. */
+    @SerialName("in_reply_to_id") val inReplyToId: String?,
+
+    /** A list of the IDs for media attachments. */
+    @SerialName("media_ids") val mediaIds: List<String>?,
+
+    /** Whether this status has been marked as sensitive and needs a content warning. */
+    val sensitive: Boolean?,
+
+    /** The spoiler text and/or content warning for this post, if it has been marked as sensitive. */
+    @SerialName("spoiler_text") val spoilerText: String?,
+
+    /** The visibility of the status. */
+    val visibility: String,
+
+    /** An ISO-8601 string that states when this status was scheduled. */
+    @SerialName("scheduled_at") val scheduledAt: String?,
+
+    /** The ID of the application used to post this status. */
+    @SerialName("application_id") val applicationId: String
+)
