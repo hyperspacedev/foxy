b76e43bd5acd9d10ae2707e18a7767147a1a6bd4
Marquis Kurt <software@marquiskurt.net>
2022-04-16 16:31:51 -0400

    Remove redundant copyright header
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- src/commonMain/kotlin/utils/FoxyRequestBuilder.kt
+++ src/commonMain/kotlin/utils/FoxyRequestBuilder.kt
@@ -10,17 +10,7 @@
  * NPL for details.
  */
 
-package utils/*
- * utils.FoxyRequestBuilder.kt
- * Copyright (C) 2022 Hyperspace Developers.
- * This file is part of project Foxy.
- *
- * The Foxy project is non-violent software: you can use, redistribute, and/or modify it under the terms of the NPLv7+
- * as found in the LICENSE file in the source code root directory or at <https://git.pixie.town/thufie/npl-builder>.
- *
- * The Foxy project comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law.  See the
- * NPL for details.
- */
+package utils
 
 import io.ktor.http.*
 
