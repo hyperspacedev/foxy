3cc6c07014a179632e3d0f69e17105910fc8232b
Marquis Kurt <software@marquiskurt.net>
2022-04-16 21:03:42 -0400

    Rename Validation.kt to ValidatedSession.kt
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- src/commonMain/kotlin/security/ValidatedSession.kt
+++ src/commonMain/kotlin/security/ValidatedSession.kt
@@ -1,5 +1,5 @@
 /*
- * Validation.kt
+ * ValidatedSession.kt
  * Copyright (C) 2022 Hyperspace Developers.
  * This file is part of project Foxy.
  *
@@ -10,17 +10,7 @@
  * NPL for details.
  */
 
-package security/*
- * Validation.kt
- * Copyright (C) 2022 Hyperspace Developers.
- * This file is part of project Foxy.
- *
- * The Foxy project is non-violent software: you can use, redistribute, and/or modify it under the terms of the NPLv7+
- * as found in the LICENSE file in the source code root directory or at <https://git.pixie.town/thufie/npl-builder>.
- *
- * The Foxy project comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law.  See the
- * NPL for details.
- */
+package security
 
 import kotlinx.datetime.toInstant
 import kotlin.experimental.xor
